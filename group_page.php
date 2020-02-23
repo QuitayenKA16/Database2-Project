5580869dac490a1c9b46a336420b69da6bb56197
Karamel Astrid Quitayen <QuitayenKA16@gmail.com>
2020-02-22 20:47:47 -0500

    added group page

---
+++ group_page.php
@@ -0,0 +1,71 @@
+<html>
+	<style>
+		* {
+			box-sizing: border-box;
+		}
+		.column1{
+			border-style: solid;
+			float: left;
+			padding: 10px;
+			height:300px;
+			margin-top:15px;
+			width:50%;
+		}
+		p.p1 {
+			font: normal 14px Verdana, Arial, sans-serif;
+			margin-left: 10px;
+		}
+	</style>
+
+	<body>
+		<?php
+			include "header.php";
+			$_SESSION['back'] = "view_groups_page.php";
+			unset($_SESSION['error']);
+			$_SESSION['table_view'] = 'default';
+			$_SESSION['table_sort'] = 'idAsc';
+			
+			$myconnection = mysqli_connect('localhost', 'root', '') or die ('Could not connect: ' . mysql_error());
+			$mydb = mysqli_select_db ($myconnection, 'db2') or die ('Could not select database');
+			$query = "SELECT * FROM groups WHERE gid = $_POST[gid]";
+			$row = mysqli_fetch_array ($result, MYSQLI_ASSOC);
+		?>
+		
+		<div class="column1" style="background-color:#f2f2f2;">
+			<h3 align="center">User Information</h3>
+			<p class="p1"><b>GID: </b> <?php echo "$_SESSION[lastName], $_SESSION[firstName]"; ?> <br>
+			<p class="p1"><b>Email: </b> <?php echo "$_SESSION[email]"; ?> <br>
+			<p class="p1"><b>Phone Number: </b> <?php echo "$_SESSION[phone]"; ?> <br>
+			<p class="p1"><b>User type: </b>
+				<?php
+					if ($_SESSION['type'] == 0)
+						echo "Parent<br>";
+					else if ($_SESSION['type'] == -1)
+						echo "Admin<br>";
+					else{ //student
+						echo "Student<br><p class='p1'><b>Grade Level: </b>$_SESSION[grade]<br>";
+						echo "<p class='p1'><b>Parent UID: </b>";
+						
+						$myconnection = mysqli_connect('localhost', 'root', '') or die ('Could not connect: ' . mysql_error());
+						$mydb = mysqli_select_db ($myconnection, 'db2') or die ('Could not select database');
+						$query = "SELECT * FROM users u, students s WHERE u.uid = $_SESSION[uid] AND u.uid = s.uid";
+						$result = mysqli_query($myconnection, $query) or die ('Query failed: ' . mysql_error());
+						$row = mysqli_fetch_array ($result, MYSQLI_ASSOC);
+						echo "$row[pid]";
+					};
+				?>
+			<br><br><br>
+			<a href='http://localhost/Database2-Project/edit_user_form.php'>Edit Details</a>
+		</div>
+		
+		<div class="column1" style="">
+			<h3 align="center">Actions</h3>
+			<?php
+				echo "<div align='center'>";
+				echo "<a href='http://localhost/Database2-Project/view_users_page.php'>View users</a><br>";
+				echo "<a href='http://localhost/Database2-Project/view_groups_page.php'>View groups</a><br><br>";
+				echo "</div>";
+			?>
+		</div>
+	</body
+</html>
--- view_groups_page.php
+++ view_groups_page.php
@@ -13,28 +13,59 @@
 	<body>
 		<?php
 			include "header.php";
+			$_SESSION['back'] = "view_groups_page.php";
+			if (isset($_POST['table_sort']))
+				$_SESSION['table_sort'] = $_POST['table_sort'];
 		?>
 
+		<form action="view_groups_page.php" method="post">
 		<table style='width:100%'>
 			<div align='center'>
 			<h3>View Groups</h3>
+			</div>
 			<?php
 				$myconnection = mysqli_connect('localhost', 'root', '') or die ('Could not connect: ' . mysql_error());
 				$mydb = mysqli_select_db ($myconnection, 'db2') or die ('Could not select database');
-				$query = "SELECT * FROM groups";
+				$query = "SELECT * FROM groups ORDER BY ";
+				
+				if ($_SESSION['table_sort']=='idAsc') $query .= "gid ASC";
+				if ($_SESSION['table_sort']=='idDes') $query .= "gid DESC";
+				if ($_SESSION['table_sort']=='nameAsc') $query .= "name ASC";
+				if ($_SESSION['table_sort']=='nameDes') $query .= "name DESC";
+				if ($_SESSION['table_sort']=='gradeAsc') $query .= "gradeLvl ASC";
+				if ($_SESSION['table_sort']=='gradeDes') $query .= "gradeLvl DESC";
+				
+				echo $query . "<br>";
 				$result = mysqli_query($myconnection, $query) or die ('Query failed: ' . mysql_error());
 				$count = mysqli_num_rows($result);
-				echo "<tr><th>GID</th><th>Name</th><th>Description</th><th>Grade Level</th></tr>";
+				
+				echo "<tr><th>GID <button type='submit' class='class2' name='table_sort' ";
+				if ($_SESSION['table_sort']=='idAsc') echo " value='idDes'>&#9660</button></th>";
+				else echo " value='idAsc'>&#9650</button></th>";
+				
+				echo "<th>Name <button type='submit' class='class2' name='table_sort' ";
+				if ($_SESSION['table_sort']=='nameAsc') echo " value='nameDes'>&#9660</button></th>";
+				else echo " value='nameAsc'>&#9650</button></th>";
+				
+				echo "<th>Description</th>";
+				
+				echo "<th>Grade Level <button type='submit' class='class2' name='table_sort' ";
+				if ($_SESSION['table_sort']=='gradeAsc') echo " value='gradeDes'>&#9660</button></th>";
+				else echo " value='gradeAsc'>&#9650</button></th>";
+				
+				echo "<th>Meetings</th></tr></form>";
+				
 				while ($row = mysqli_fetch_array ($result, MYSQLI_ASSOC)) {
 					echo "<tr>";
+					echo "<form action='http://localhost/Database2-Project/group_page.php' method='post'>";
 					echo "<td>$row[gid]</td>";
 					echo "<td>$row[name]</td>";
 					echo "<td>$row[description]</td>";
 					echo "<td>$row[gradeLvl]</td>";
-					echo "</tr>";
+					echo "<td align='center'><button type='submit' name='edit_uid' value='$row[gid]'>VIEW</button>";
+					echo "</td></form></tr>";
 				}
 			?>
-			</div>
 		</table>
 	</body
 	
